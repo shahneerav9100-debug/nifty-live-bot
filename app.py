dd5afff200a79178a53b0e026e1d123219476177
shahneerav9100-debug <shahneerav9100@gmail.com>
2026-01-06 23:52:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot logic is triggered!"
+    return "The Web Server is Online!"
 
 if __name__ == "__main__":
-    # This starts your trading logic in the background
+    # This starts the run_bot function from main.py
     threading.Thread(target=main.run_bot, daemon=True).start()
-    # Render uses port 10000 by default
+    
+    # This starts the web server for Render/Cron-job
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
